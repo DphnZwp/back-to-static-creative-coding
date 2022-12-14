c87648160e00b796f5678b766fd619fbdb3f8414
DphnZwp <zwuupdaphne@gmail.com>
2022-12-14 10:56:39 +0100

    added bubblegum svg

--- src/routes/bubblegum/+page.svelte
+++ src/routes/bubblegum/+page.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <main on:mousemove={move}>
-  <svg style="transform: translate({position.x}px,{position.y}px)" width="128" height="147" viewBox="0 0 128 147" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg class="bubblegum__magnet" style="transform: translate({position.x}px,{position.y}px)" width="128" height="147" viewBox="0 0 128 147" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g filter="url(#filter0_d_44_231)">
     <path d="M31 99H23C17.4772 99 13 103.477 13 109V122C13 127.523 17.4772 132 23 132H31C36.5228 132 41 127.523 41 122V109C41 103.477 36.5228 99 31 99Z" fill="white" stroke="white" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M115 99H107C101.477 99 97 103.477 97 109V122C97 127.523 101.477 132 107 132H115C120.523 132 125 127.523 125 122V109C125 103.477 120.523 99 115 99Z" fill="white" stroke="white" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
@@ -29,15 +29,50 @@
   <section class="bubblegum">
     <h1><span>Bubblegum</span> Magnet</h1>
   </section>
-  
+
+  <svg width="1385" height="393" viewBox="0 0 1385 393" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="774.143" cy="161.112" r="35" transform="rotate(8.84286 774.143 161.112)" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="112.5" cy="245.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="160.5" cy="264.5" r="28" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="707.94" cy="150.812" r="28" transform="rotate(8.84286 707.94 150.812)" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1144.5" cy="174.5" r="28" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="812.5" cy="88.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="471.5" cy="125.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="278.5" cy="216.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="339.5" cy="168.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="691.5" cy="253.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="557.5" cy="178.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1274.5" cy="91.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="427.5" cy="37.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="473.5" cy="190.5" r="26" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="502.5" cy="54.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="444.5" cy="234.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="289.5" cy="372.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="113.5" cy="348.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="667.5" cy="347.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="988.5" cy="291.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1107.5" cy="408.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1116.5" cy="279.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="936.5" cy="393.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1347.5" cy="401.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1245.5" cy="216.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="988.5" cy="106.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="792.5" cy="243.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="883.5" cy="185.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1151.5" cy="129.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1207.5" cy="358.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="1245.5" cy="346.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="521.5" cy="359.5" r="22" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="849.5" cy="318.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="710.5" cy="346.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="403.5" cy="364.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="37.5" cy="310.5" r="35" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="546" cy="268" r="23.5" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    <circle cx="963" cy="216" r="23.5" fill="#E89398" stroke="#BA3A51" stroke-width="5"/>
+    </svg>    
 </main>
 
 <style>
-
-  main {
-    width: 100vw;
-    height: 100vh;
-  }
   :global(header a) {
     color: black;
   }
@@ -66,7 +101,8 @@
     -webkit-text-stroke: 1px white;
   }
 
-  svg {
+  .bubblegum__magnet {
     position: absolute;
+    z-index: 1;
   }
 </style>
